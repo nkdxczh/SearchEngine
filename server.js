c5a5811ae05beeef5e15cfed885f2cf72f6e7f54
nateg10 <natepgyory@gmail.com>
2017-04-16 12:41:21 -0400

    query works for bulk data

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 var express = require('express');
 var path = require('path');
 var url = require('url');
+var request = require('request');
 var app = express();
 
 
 //---------------------------------------------------------------------------------------------
 //                                              Port Information
 var PORT = 3333;
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log('listening on PORT 3333');
 });
 
@@ -20,14 +21,27 @@ app.use(express.static('public'));
 //-----------------------------------------------------------------------------------------------
 //                                              Routing Information
 
-app.get('/query', (req, res) =>{
+app.get('/query', (req, res) => {
     //var queryObject = url.parse(req.url, true);
     //var path = queryObject.path;
     //var params = queryObject.query;
     console.log('path is: ' + req.url);
+    //call elasticsearch api
+
     //console.log('params are: ' + params);
-    res.writeHead(200);
-    res.write("got it");
-    res.end();
+    request("http://localhost:9200/_search?pretty", {
+        "query": {
+            "match": {
+                "column": "year"
+            }
+        }
+    }, (error, response, body) => {
+        console.log('error:', error); // Print the error if one occurred 
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.writeHead(200);
+        res.write(body);
+        res.end();
+    });
 });
 
